2e77d64ee996d27040dc54439af28adea43ee021
Bera Senol <berasenol@icloud.com>
2024-05-23 00:55:39 +0200

    index page completed

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,55 @@
 <template>
-  <div>Index</div>
+  <UContainer class="bg-white-50 text-black-900 rounded-lg mt-8 text-center">
+    <UContainer class="flex justify-center pt-8">
+      <img
+        src="/assets/logo.jpg"
+        alt="Logo Entrepot del Tartufo"
+        width="328px"
+      />
+    </UContainer>
+    <p class="text-lg py-8 pt-12 px-16">
+      Van harte welkom op onze website! Restaurant Entrepot del Tartufo is
+      gevestigd in een oude opslagplaats in het gezellige hemelrijk straatje te
+      Hasselt.<br />
+      In een ongedwongen sfeer kan u genieten van de puurheid van de Italiaanse
+      keuken met een touch of truffle in combinatie met steengoede wijnen.
+    </p>
+    <p class="text-3xl pt-4 italic">Lunch @ Entrepot del Tartufo</p>
+    <p class="text-lg pt-4 px-16">
+      Kom genieten van onze heerlijke lunch! Lunchmenu varieert van week tot
+      week. <br />
+      Maandag | Donderdag | Vrijdag <br />
+      2 Gangen 30 EUR <br />
+      3 Gangen 40 EUR <br />
+    </p>
+    <UButton
+      color="black"
+      size="xl"
+      variant="link"
+      to="reservation"
+      class="text-lg"
+    >
+      Klik Hier Om Te Reserveren
+    </UButton>
+    <p class="text-3xl pb-4 py-8 italic">Parmezaanbus @ CASA</p>
+    <p class="text-lg px-16">
+      De Parmezaanbus brengt de restaurant-experience bij jouw thuis. Naast
+      gerechten die op te warmen zijn bereiden wij op jouw oprit à la minute
+      Tagliolini al Tartufo – pasta gedraaid in een grote Parmzaanse kaas
+      afgetopt met verse geraspte truffel. Donderdag t.e.m. maandag | Regio
+      Hasselt | Levering en bereiding duurt 30min. tussen 18u en 20u | vanaf 6
+      personen
+    </p>
+    <UButton
+      color="black"
+      size="xl"
+      variant="link"
+      to="reservation"
+      class="text-lg pb-8"
+    >
+      Reserveer de Parmezaanbus
+    </UButton>
+  </UContainer>
 </template>
 
 <script lang="ts" setup></script>
