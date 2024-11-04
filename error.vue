6d112782d124d55c3ea6fa0d67459a96e8a18de4
Bera Senol <bleuberry2be@icloud.com>
2024-11-04 23:27:18 +0100

    error page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-slate-950 min-h-screen">
+        <NuxtLayout>
+            <h1>{{ error?.statusCode }}</h1>
+            <NuxtLink to="/">Go back home</NuxtLink>
+        </NuxtLayout>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+    error: Object as () => NuxtError
+})
+</script>
