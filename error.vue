6cfdcff6b639ecea03b0716893b1ea32d6cd8d96
Bera Senol <bleuberry2be@icloud.com>
2024-11-24 20:48:04 +0100

    Error Page

--- error.vue
+++ error.vue
@@ -1,8 +1,11 @@
 <template>
-    <div class="bg-slate-950 min-h-screen">
+    <div class="bg-gray-50 dark:bg-gray-950 min-h-screen text-center">
         <NuxtLayout>
-            <h1>{{ error?.statusCode }}</h1>
-            <NuxtLink to="/">Go back home</NuxtLink>
+            <h1 class="text-7xl font-bold tracking-wider pt-60">Error: {{ error?.statusCode }}</h1>
+            <h1 class="text-6xl font-light tracking-wide py-12">{{ error?.statusMessage }}</h1>
+            <UButton class="bg-gray-100 dark:bg-gray-900 text-3xl tracking-wide px-5 mb-32" to="/">
+                Back Home
+            </UButton>
         </NuxtLayout>
     </div>
 </template>
