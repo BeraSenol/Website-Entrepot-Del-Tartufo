cc8c9949c0606dfc6539376dbdd7b4702b7aed51
Bera Senol <bleuberry2be@icloud.com>
2024-12-21 15:15:09 +0100

    Contact form Clean Up

--- components/Contact/Form.vue
+++ components/Contact/Form.vue
@@ -1,5 +1,5 @@
 <template>
-  <UForm :schema="schema" :state="state" class="space-y-8" name="contact-form">
+  <form :schema="schema" :state="state" class="space-y-8" name="contact-form">
     <UFormGroup label="Naam" name="name" required size="xl" :ui="{
       label: {
         base: 'text-lg',
@@ -12,7 +12,7 @@
         base: 'text-lg',
       },
     }">
-      <UInput v-model="state.email" name="email" type="email" placeholder="voorbeeld@domein.be"/>
+      <UInput v-model="state.email" name="email" type="email" placeholder="voorbeeld@domein.be" />
 
     </UFormGroup>
     <UFormGroup name="textarea" label="Opmerking" required size="xl" :ui="{
@@ -25,13 +25,12 @@
     <UButton type="submit" class="bg-gray-100 dark:bg-gray-900 border-gray-200 dark:border-gray-800 user-button">
       Verzend
     </UButton>
-  </UForm>
+  </form>
 </template>
 
 <script setup lang="ts">
 import { object, string, type InferType } from "yup";
 import { reactive, ref } from "vue";
-import type { FormSubmitEvent } from '#ui/types'
 
 
 const schema = object({
@@ -40,16 +39,10 @@ const schema = object({
   textarea: string().required("Verplicht!"),
 });
 
-type Schema = InferType<typeof schema>;
 
 const state = reactive({
-  email: undefined,
-  name: undefined,
-  textarea: undefined,
+  email: String,
+  name: String,
+  textarea: String,
 });
-
-async function onSubmit(event: FormSubmitEvent<Schema>) {
-  // Do something with event.data
-  console.log(event.data)
-}
 </script>
